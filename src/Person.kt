ff579e66470441f92f2bdc8c4466af647091798e
Affan Zanuar <affanzanuar1@gmail.com>
2022-07-04 08:29:13 +0700

    Menambahkan fitur health untuk mengakhiri game

--- src/Person.kt
+++ src/Person.kt
@@ -1,14 +1,33 @@
 abstract class Person {
     open lateinit var name : String
     open var score : Int = 0
+    open var health : Double = 100.00
     abstract fun playerHand() : String
 
     private var a = HandType.A.nama
     private var b = HandType.B.nama
     private var c = HandType.C.nama
 
+    private var healthDraw = arrayOf(1.5 , 2.5 , 3.5 , 4.5)
+    private var randomHealthPlayer1 = healthDraw.random()
+    private var randomHealthOpponent = healthDraw.random()
+
+    private var healthWinOrLose = arrayOf(31.5 , 35.5 , 37.5 )
+    private var randomHealthWinOrLose = healthWinOrLose.random()
+
     fun attack(opponent : Person){
-        println("\nSaatnya ${this.name} melawan ${opponent.name}")
+        println("\nSaatnya ${this.name} melawan ${opponent.name}\n")
+
+        fun printLiveResult(){
+            println("""
+                ------ PLAYER STATS ------
+                Name   : ${this.name}   || ${opponent.name}
+                Win    : ${this.score}       || ${opponent.score}
+                Health : ${this.health}   || ${opponent.health}
+                ==========================
+            """.trimIndent())
+        }
+        printLiveResult()
 
         val playerOneHand = this.playerHand()
         val playerTwoHand = opponent.playerHand()
@@ -21,18 +40,12 @@ abstract class Person {
             """.trimIndent())
         }
 
-        fun printLiveScore(){
-            println("""
-                --------- SCORE ----------
-                ${this.name} = ${this.score} || ${opponent.name} = ${opponent.score}
-                ==========================
-            """.trimIndent())
-        }
-
         if (playerOneHand==playerTwoHand){
             printHandResult()
             println("\n||******** DRAW ********||\n")
-            printLiveScore()
+            this.health += randomHealthPlayer1
+            opponent.health += randomHealthOpponent
+            printLiveResult()
         } else if (
             playerOneHand == a && playerTwoHand == b
             || playerOneHand == b && playerTwoHand == c
@@ -41,12 +54,48 @@ abstract class Person {
             printHandResult()
             println("\n||****** ${this.name} WIN ******||\n")
             this.score++
-            printLiveScore()
+            this.health += randomHealthPlayer1
+            opponent.health -= randomHealthWinOrLose
+            printLiveResult()
         } else {
             printHandResult()
             println("\n||****** ${opponent.name} WIN ******||\n")
+            opponent.health += randomHealthOpponent
+            this.health -= randomHealthWinOrLose
             opponent.score++
-            printLiveScore()
+            printLiveResult()
+        }
+
+        if (this.health <=0 || opponent.health <=0){
+            Utility.getHeaderGame()
+            println("""
+                
+                    ******************
+                |####### GAME OVER #######|
+                    ******************
+                      
+            """.trimIndent())
+
+            if (this.health<=0){
+                println("""
+                         ${opponent.name}
+                          WIN THIS MATCH
+                    ~~~~~~~~~~~~~~~~~~~~~~~~~
+                    
+                """.trimIndent())
+            } else {
+                println("""
+                         ${this.name}
+                          WIN THIS MATCH
+                    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    
+                """.trimIndent())
+            }
+
+            this.health = 100.00
+            opponent.health = 100.00
+            this.score = 0
+            opponent.score = 0
         }
     }
 }
